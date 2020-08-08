850d797f72e980b14bbc8f198e900f4fa2d95e54
nanthu0123 <nanthu0123@gmail.com>
2020-08-08 22:38:20 +0530

    postgresql db system connected

--- employee_register/settings.py
+++ employee_register/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'employee_register.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'employeedb',
+        'USER': 'postgres',
+        'PASSWORD': 'nanthu23',
+        'HOST': 'localhost'
     }
 }
 
@@ -119,4 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
