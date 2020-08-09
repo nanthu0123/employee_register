b3c8e7f5e9ba0d6d61c9adeb7bef2028f79b69e5
nanthu0123 <nanthu0123@gmail.com>
2020-08-09 21:28:25 +0530

    funcions created for form and list

--- register/views.py
+++ register/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import EmployeeForm
+from .models import Employee
 
 def employee_form(request):
-    return render(request,'register/employee_form.html')
+    if request.method=='GET':
+        context={'form':EmployeeForm()}
+        return render(request,'register/employee_form.html',context)
+    else:
+        form=EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('list')
 
 def employee_list(request):
-    return render(request,'register/employee_list.html')
+    context={'employee_list':Employee.objects.all()}
+    return render(request,'register/employee_list.html',context)
 
 def employee_delete(request):
     return
