57a181f16f083223f65bdfe2304ba488e82e0827
nanthu0123 <nanthu0123@gmail.com>
2020-08-09 22:06:38 +0530

    update urlpath added

--- register/urls.py
+++ register/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('',views.employee_form),
-    path('list',views.employee_list,name='list'),
-    path('delete',views.employee_delete)
+urlpatterns = [
+    path('', views.employee_form, name='insert'),
+    path('<int:id>/', views.employee_form, name='update'),
+    path('list', views.employee_list, name='list'),
+    path('delete', views.employee_delete)
 ]
