3874f020133caf3965d757f745cb57f8faf93344
nanthu0123 <nanthu0123@gmail.com>
2020-08-10 22:50:22 +0530

    urls modified

--- register/urls.py
+++ register/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form, name='insert'),
+    path('insert', views.employee_form, name='insert'),
     path('<int:id>/', views.employee_form, name='update'),
-    path('list', views.employee_list, name='list'),
-    path('delete', views.employee_delete)
+    path('', views.employee_list, name='list'),
+    path('delete/<int:id>', views.employee_delete, name='delete')
 ]
