071d01863491d9940d7769c9bcf17cdff17c396e
nanthu0123 <nanthu0123@gmail.com>
2020-08-10 22:52:41 +0530

    delete function created

--- register/views.py
+++ register/views.py
@@ -1,30 +1,35 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import EmployeeForm
 from .models import Employee
 
-def employee_form(request,id=0):
-    if request.method=='GET':
-        if id==0:
-            form=EmployeeForm()
-            context={'form':form}
+
+def employee_form(request, id=0):
+    if request.method == 'GET':
+        if id == 0:
+            form = EmployeeForm()
+            context = {'form': form}
         else:
-            employee=Employee.objects.get(pk=id)
-            form=EmployeeForm(instance=employee)
-            context={'form':form}
-        return render(request,'register/employee_form.html',context)
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(instance=employee)
+            context = {'form': form}
+        return render(request, 'register/employee_form.html', context)
     else:
-        if id==0:
-            form=EmployeeForm(request.POST)
+        if id == 0:
+            form = EmployeeForm(request.POST)
         else:
-            employee=Employee.objects.get(pk=id)
-            form=EmployeeForm(request.POST,instance=employee)
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
         return redirect('list')
 
-def employee_list(request):
-    context={'employee_list':Employee.objects.all()}
-    return render(request,'register/employee_list.html',context)
 
-def employee_delete(request):
-    return
+def employee_list(request):
+    context = {'employee_list': Employee.objects.all()}
+    return render(request, 'register/employee_list.html', context)
+
+
+def employee_delete(request, id):
+    employee = Employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('list')
